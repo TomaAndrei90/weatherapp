1e794d87f71b15f45ea335eb5aa98f4f370b8a70
Andrei Toma <toma.andrei90@yahoo.com>
2020-06-03 15:34:59 +0300

    API + bigger mock.json

--- client/src/API/API.js
+++ client/src/API/API.js
@@ -1,7 +1,9 @@
 import mock from './mock.json';
 
-const getWeather = () => {
+const getWeather = async () => {
   return mock;
+  // const result = await fetch('https://api.openweathermap.org/data/2.5/onecall?lat=44.4268&lon=26.1025&appid=${apikey}');
+  // return await result.json();  
 };
 
 export {
