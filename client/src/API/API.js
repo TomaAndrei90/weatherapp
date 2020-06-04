a42ffc8577643498dcf95951a238c27ae15cc0a5
Andrei Toma <toma.andrei90@yahoo.com>
2020-06-04 15:04:57 +0300

    cards css + replacing react-icons with weather-icons

--- client/src/API/API.js
+++ client/src/API/API.js
@@ -1,8 +1,9 @@
 import mock from './mock.json';
 
 const getWeather = async () => {
-  return mock;
-  // const result = await fetch('https://api.openweathermap.org/data/2.5/onecall?lat=44.4268&lon=26.1025&appid=${apikey}');
+	return mock;
+	// const apiKey = ''
+  // const result = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=44.4268&lon=26.1025&units=metric&appid=${apiKey}`);
   // return await result.json();  
 };
 
--- client/src/components/WeatherCard.js
+++ client/src/components/WeatherCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './WeatherCard.css';
+import '../css/weather-icons.min.css';
 
 const WeatherCard = ({ 
     type,
@@ -10,9 +11,9 @@ const WeatherCard = ({
     feels_like,
     weather,
   }) => {
-    const { icon, description } = weather[0];
-    const OPEN_WEATHER_IMG_URL = 'http://openweathermap.org/img/wn/';
-    const iconSrc = `${OPEN_WEATHER_IMG_URL}${icon}.png`;
+    const { id: weatherId, icon, description } = weather[0];
+    // const OPEN_WEATHER_IMG_URL = 'http://openweathermap.org/img/wn/';
+    // const iconSrc = `${OPEN_WEATHER_IMG_URL}${icon}.png`;
 
     let displayTime = 'placeholder time';
     const date = new Date(dt * 1000);
@@ -39,11 +40,15 @@ const WeatherCard = ({
     const weatherCard = (
       <div className={`WeatherCard WeatherCard--${type}`}>
         { type === 'current' && <div className="overlay"></div> }
+				{ type === 'current' && <div className="overlay-dark"></div> }
 				{ type === 'current' && <h3 className="WeatherCard__city">{ timezone } </h3> }
         <div className="WeatherCard__time">{ displayTime }</div>
-        <div className="WeatherCard__icon"><img src={ iconSrc } alt="Weather Icon" /></div>
-        { (type === 'current' || type === 'hourly') && <div className="WeatherCard__temp">{ temp }</div> }
-        { type === 'daily' && <div><span className="WeatherCard__temp">{ temp.max }</span> / { temp.min }</div> }
+        <div className="WeatherCard__icon">
+					{/* <img src={ iconSrc } alt="Weather Icon" /> */}
+					<i className={`wi wi-owm-${weatherId}`}></i>
+					</div>
+        { (type === 'current' || type === 'hourly') && <div className="WeatherCard__temp"><span>{ temp }</span><i className="wi wi-celsius"></i></div> }
+        { type === 'daily' && <div><span className="WeatherCard__temp">{ temp.max }<i className="wi wi-celsius"></i></span> <span className="WeatherCard__min">/ { temp.min }</span></div> }
         <div className="WeatherCard__description">{ description }</div>
         { type === 'current' && <div className="WeatherCard__feel">Real Feel: { feels_like }</div> }
       </div>
